d5cbc75009d7abe2e8043f524d1b192bfe44acfd
shangdian.han <shangdian.han@2861.wiki>
2022-06-13 19:05:54 +0800

    DataFrameFile & Job

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DataFrameFile(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    cached_file = models.FileField()
+
+
+class Job(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    input_data_frame = models.ForeignKey(
+        DataFrameFile, on_delete=models.CASCADE, related_name="input", null=True
+    )
+    output_data_frame = models.ForeignKey(
+        DataFrameFile, on_delete=models.CASCADE, related_name="output"
+    )
