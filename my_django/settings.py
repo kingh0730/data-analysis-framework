ee3fad2852bd72a2b9ada75a61b71105a1540613
shangdian.han <shangdian.han@2861.wiki>
2022-06-13 19:19:17 +0800

    nice

--- my_django/settings.py
+++ my_django/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Media
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
