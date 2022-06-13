471c952390534c17030b4e1d71288fe6fc18c29b
shangdian.han <shangdian.han@2861.wiki>
2022-06-13 17:57:07 +0800

    typing

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'base/home.html')
