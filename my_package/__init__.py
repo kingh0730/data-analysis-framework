64b84626cdca7c6e6c7ae68db9ed5e29c65af83e
shangdian.han <shangdian.han@2861.wiki>
2022-06-13 13:55:03 +0800

    calc_value_ontology

--- my_package/__init__.py
+++ my_package/__init__.py
@@ -1,1 +1,54 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+
+import enum
+
+
+class ValueOntology(enum.Enum):
+    UNDETERMINED = 0
+    QUANTITY = 1
+    PERCENTAGE = 2
+    DIFF_QUANTITY = 3
+    DIFF_PERCENTAGE = 4
+
+
+def calc_value_ontology(name: str, unit: str) -> ValueOntology:
+    quantity_positions = [
+        name.rfind("数量"),
+        name.rfind("个数"),
+        name.rfind("数"),
+        name.rfind("量"),
+        name.rfind("次"),
+        name.rfind("分"),
+        name.rfind("级"),
+    ]
+
+    percentage_positions = [
+        name.rfind("占比"),
+        name.rfind("比重"),
+        name.rfind("比"),
+    ]
+
+    diff_positions = [
+        name.rfind("增长"),
+        name.rfind("增"),
+    ]
+
+    all_positions = quantity_positions + percentage_positions + diff_positions
+    all_positions.sort()
+    last = all_positions[-1]
+
+    unit_is_percentage = unit in ["%", "‰"]
+
+    if last == -1:
+        return ValueOntology.UNDETERMINED
+    if last in quantity_positions and (not unit_is_percentage):
+        return ValueOntology.QUANTITY
+    if last in percentage_positions and unit_is_percentage:
+        return ValueOntology.PERCENTAGE
+    if last in diff_positions and (not unit_is_percentage):
+        return ValueOntology.DIFF_QUANTITY
+    if last in diff_positions and unit_is_percentage:
+        return ValueOntology.DIFF_PERCENTAGE
+
+    return ValueOntology.UNDETERMINED
--- tests/test_my_package.py
+++ tests/test_my_package.py
@@ -1,5 +1,16 @@
-from my_package import __version__
+import my_package
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_version() -> None:
+    assert my_package.__version__ == "0.1.0"
+
+
+def test_calc_value_ontology() -> None:
+    test = my_package.calc_value_ontology("餐饮服务；外国餐厅；此类数据总个数", "个")
+    assert test == my_package.ValueOntology.QUANTITY
+
+    test = my_package.calc_value_ontology("空气质量优良天数比例", "%")
+    assert test == my_package.ValueOntology.PERCENTAGE
+
+    test = my_package.calc_value_ontology("近2年本地注册企业数量增长", "个")
+    assert test == my_package.ValueOntology.DIFF_QUANTITY
