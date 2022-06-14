d8ca72fc4b8e84046a9df9870b4b3c81a678c54d
shangdian.han <shangdian.han@2861.wiki>
2022-06-14 11:26:00 +0800

    month

--- base/models.py
+++ base/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 
 
+class Month(models.Model):
+    month_int = models.PositiveSmallIntegerField()
+    month_str = models.CharField(max_length=7)
+
+
 class DataFrameFile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     cached_file = models.FileField()
